0498025a355c202cd70cd6461dab192f95df20b4
Brianonyango879 <brianonyango879@gmail.com>
2025-05-14 12:56:03 +0300

    made changes at serializers.py

--- users/serializers.py
+++ users/serializers.py
@@ -25,19 +25,3 @@ class UserSerializer(serializers.ModelSerializer):
         return super().update(instance, validated_data)
 
 
-    def validate(self, data):
-        if data['password'] != data['confirm_password']:
-            raise serializers.ValidationError("Passwords do not match.")
-        return data
-
-    def validate_confirm_password(self, value):
-        data = self.get_initial()
-        if value != data.get('password'):
-            raise serializers.ValidationError("Passwords do not match.")
-        return value
-
-    def validate_username(self, value):
-        data = self.get_initial()
-        if value == data.get('username'):
-            raise serializers.ValidationError("Username cannot be the same.")
-
